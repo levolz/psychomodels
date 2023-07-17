af7c68948f90eb963605fce183b918d12e33a83a
levolz <leonhard.volz@gmail.com>
2023-07-17 09:11:37 +0200

    "m2m fields"

--- models/admin.py
+++ models/admin.py
@@ -4,6 +4,14 @@ from django.contrib import admin
 from . import models
 
 
+class PublicationAdmin(admin.ModelAdmin):
+    filter_horizontal = ["model_authors"]
+
+
+class FrameworkAdmin(admin.ModelAdmin):
+    filter_horizontal = ["frameworks"]
+
+
 admin.site.register(models.Author)
 admin.site.register(models.Psychmodel)
 # admin.site.register(models.Parameter)
@@ -13,6 +21,6 @@ admin.site.register(models.Modelvariable)
 admin.site.register(models.Language)
 admin.site.register(models.Framework)
 # admin.site.register(models.Psychfield)
-admin.site.register(models.Publication)
+admin.site.register(models.Publication, PublicationAdmin)
 # admin.site.register(models.Softwarepackage)
 # admin.site.register(models.Measurementinstrument)
